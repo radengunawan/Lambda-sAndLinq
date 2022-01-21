50bfeacca6657429a2fd9066aa34e9f14e4087a0
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-21 11:20:33 +0800

    Add project files.

---
+++ LambdaExpressionsAndLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LambdaExpressionsAndLinq
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 1, 2, 3, 4 };
+            var result = array.Select(i => i * 2); // from i in array select i * 2;
+            
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
